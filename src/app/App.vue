1193da906dd7ce79b1445d8c1a571df5d7850ed4
Vashka-code <ivantrushkof@gmail.com>
2024-06-05 23:39:52 +0300

    initial commit + small setup

---
+++ src/app/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">Content</main>
+</template>
+
+<style scoped>
+.main {
+  height: 100dvh;
+  width: 100%;
+  background-color: #0c0e1c;
+  background-image: url('./assets/bg.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+</style>
---
+++ src/shared/components/FlipCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flipcard">
+    <div class="flipcard__front"></div>
+    <div class="flipcard__back"></div>
+  </div>
+</template>
+
+<style scoped>
+.flipcard {
+  width: 185px;
+  height: 276px;
+}
+</style>
