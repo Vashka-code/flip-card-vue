fd49fa0beb1df4bcd4e39038865c8785d8cb6701
Vashka-code <ivantrushkof@gmail.com>
2024-06-06 23:00:31 +0300

    and main =)

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import 'reset-css'
 import './main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './app/App.vue'
 
 const app = createApp(App)
-
-app.use(createPinia())
 
 app.mount('#app')
